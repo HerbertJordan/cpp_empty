9f8890f50eccaa7d9c78b5c2ccee508e9e0abdca
Herbert Jordan <herbert@dps.uibk.ac.at>
2014-06-16 19:49:23 +0200

    Added basic setup.

---
+++ code/cmake/environment.cmake
@@ -0,0 +1,99 @@
+#
+# This file sets up some general variables and include paths for the build environment
+#
+
+# -------------------------------------------------------------- define some code locations
+
+ENABLE_LANGUAGE(C)
+ENABLE_LANGUAGE(CXX)
+
+# setup std-include directories (to support some IDEs)
+if (GCC_INCLUDE_DIR) 
+	include_directories( ${GCC_INCLUDE_DIR} )
+endif()
+
+# get code root directory (based on current file name path)
+get_filename_component( project_code_dir ${CMAKE_CURRENT_LIST_FILE} PATH )
+get_filename_component( project_root_dir ${project_code_dir} PATH )
+
+include (${project_code_dir}/lookup_lib.cmake)
+include (${project_code_dir}/add_unit_test.cmake)
+
+set ( project_inc_dir 	            	${project_code_dir}/include )
+set ( project_src_dir 	            	${project_code_dir}/src )
+
+# -------------------------------------------------------------- determines project version
+
+find_package(Git)
+if(GIT_FOUND)
+	# deduce the code version using git describe
+	set ( project_version "`(cd ${project_code_dir}; ${GIT_EXECUTABLE} describe --dirty --always)`")
+	#set ( project_version "shit" )
+else()
+	set ( project_version "unknown" )
+endif()
+
+
+
+# --------------------------------------------------------------------- including libraries
+
+# set up third-part library home
+if (NOT third_part_libs_home )
+	if ( DEFINED LIBS_HOME ) 
+		set ( third_part_libs_home ${LIBS_HOME} CACHE PATH "Third part library home" )
+	else()
+		set ( third_part_libs_home $ENV{LIBS_HOME} CACHE PATH "Third part library home" )
+	endif()
+endif()
+
+# lookup Google Test libraries
+lookup_lib ( GTEST gtest )
+lookup_lib ( GTEST_MAIN gtest_main )
+
+
+# ------------------------------------------------------------- configuration for platforms
+
+# enable C++0x support within gcc (if supported)
+if (CMAKE_COMPILER_IS_GNUCXX)
+
+	# add general flags
+	add_definitions( -fshow-column )
+	add_definitions( -fdiagnostics-show-option )
+	add_definitions( -Wall )
+	add_definitions( -fopenmp )
+	add_definitions( -Wextra )
+	add_definitions( -Werror )
+	add_definitions( -pedantic )
+
+	# add flag allowing arbitrary library ordering (not default in newer distributions)
+	set (CMAKE_CXX_FLAGS "-Wl,--no-as-needed ${CMAKE_CXX_FLAGS}")
+
+	# add flags for debug mode
+	set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g3 -O0")
+  
+	# add flags for release mode
+	set (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
+
+
+	# check for -std=c++0x
+	include(CheckCXXCompilerFlag)
+	check_cxx_compiler_flag( -std=c++0x CXX0X_Support )
+	if(CXX0X_Support)
+		set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+	else()
+		message( "WARNING: --std=c++0x not supported by your compiler!" )
+	endif()
+
+	# add project version definition (add_definitions escapes back-quotes)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DPROJECT_VERSION=\"\\\"${project_version}\\\"\"")
+
+endif()
+
+
+# --------------------------------------------------------- Valgrind / GTest testing suite
+
+# add -all-valgrind target
+add_custom_target(valgrind)
+
+
+
